dd234744d4f00e6747456538fe87fdcfdc9fe0d7
mrjohanosman <mrjohanosman@gmail.com>
2015-04-14 21:25:31 +0800

    Version 3
    Added register page

---
+++ register.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/normal_page.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="styles.css" rel="stylesheet" type="text/css" />
+<title>Reversi</title>
+</head>
+
+<body>
+<div style="position:relative;top:0px;" align="center">
+  <table width="1200" border="0">
+    <tr>
+      <td width="970"><span style="text-align: left; font-size: 30pt;"><img src="imgs/left arrow.png" width="75" height="27" alt="previous" />&nbsp;<a href="news.php">Welcome to reversi!</a>&nbsp;<img src="imgs/right arrow.png" width="75" height="27" alt="next" /></span></td>
+      <td width="80"><img src="imgs/profile_icon.png" width="80" height="62" alt="profiles" /></td>
+      <td width="142"><span style="text-align: left; font-size: 30pt;"><a href="user_profiles.php">User<br />Profiles</a></span>
+      </td>
+    </tr>
+  </table>
+</div>
+<div style="position:relative;top:20px;" align="center">
+  <table width="1117" border="0">
+    <tr>
+      <td width="105"><span style="text-align: left; font-size: 30pt;"><a href="home.php">Home</a>&nbsp;</span></td>
+      <td width="10"><img src="imgs/menu div.png" width="10" height="50" /></td>
+      <td width="193"><span style="text-align: left; font-size: 30pt;">&nbsp;<a href="dashboard.php">Dashboard&nbsp;</a></span></td>
+      <td width="10"><img src="imgs/menu div.png" alt="" width="10" height="50" /></td>
+      <td width="209"><span style="text-align: left; font-size: 30pt;">&nbsp;<a href="tournament.php">Tournament</a>&nbsp;</span></td>
+      <td width="10"><img src="imgs/menu div.png" alt="" width="10" height="50" /></td>
+      <td width="156"><span style="text-align: left; font-size: 30pt;">&nbsp;<a href="ranking.php">Ranking</a>&nbsp;</span></td>
+      <td width="10"><img src="imgs/menu div.png" alt="" width="10" height="50" /></td>
+      <td width="97"><span style="text-align: left; font-size: 30pt;">&nbsp;<a href="faq.php">FAQ</a>&nbsp;</span></td>
+      <td width="10"><img src="imgs/menu div.png" alt="" width="10" height="50" /></td>
+      <td width="196"><span style="text-align: left; font-size: 30pt;">&nbsp;<a href="abtus.php">About Us</a></span></td>
+    </tr>
+  </table>
+</div>
+
+<div style="position:relative;top:65px;" align="center"><img src="imgs/reversilogo.png" width="800" height="198" alt="REVERSI" /></div>
+</div>
+<div style="position:relative;top:65px;height:1400px;" align="center"><!-- InstanceBeginEditable name="content_area" -->
+  <div class="subheader">REGISTER</div>
+  <p>
+  
+  <table width="870" border="0" align="center" class="tbl">
+    <tr>
+      <td width="476" align="right">Username </td>
+      <td width="17" height="54" align="right">&nbsp;</td>
+      <td width="272"><form action="" method="post" name="form1" class="logintxtbox" id="form1" >
+        <label for="username"></label>
+             <input name="username" type="text" class="logintxtbox" id="username" />
+      </form></td>
+      <td width="87">&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="right">Password</td>
+      <td align="right">&nbsp;</td>
+      <td><form action="" method="post" name="form2" class="logintxtbox" id="form2">
+        <label for="password"></label>
+        <input name="password" type="password" class="logintxtbox" id="password" />
+      </form></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="right">Confirm Password</td>
+      <td align="right">&nbsp;</td>
+      <td><span class="logintxtbox">
+        <input name="passwordcfnm" type="password" class="logintxtbox" id="passwordcfnm" />
+      </span></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="right">Email</td>
+      <td align="right">&nbsp;</td>
+      <td><span class="logintxtbox">
+        <input name="email" type="text" class="logintxtbox" id="email" />
+      </span></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="right">Date of Birth</td>
+      <td align="right">&nbsp;</td>
+      <td><span class="logintxtbox">
+        <input name="dob" type="text" class="logintxtbox" id="dob" />
+      </span></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="right">Gender</td>
+      <td align="right">&nbsp;</td>
+      <td><form id="form5" name="form5" method="post" action="">
+        <p class="small">
+          <label>
+            <input type="radio" name="Gender" value="Male" id="Gender_0" />
+            male</label>
+          <label>
+            <input type="radio" name="Gender" value="Female" id="Gender_1" />
+            female</label>
+          <br />
+        </p>
+      </form></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="right">Handphone</td>
+      <td align="right">&nbsp;</td>
+      <td><span class="logintxtbox">
+        <input name="handphone" type="text" class="logintxtbox" id="handphone" />
+      </span></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="right">First Name</td>
+      <td align="right">&nbsp;</td>
+      <td><span class="logintxtbox">
+        <input name="fname" type="text" class="logintxtbox" id="fname" />
+      </span></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="right">Last Name</td>
+      <td align="right">&nbsp;</td>
+      <td><span class="logintxtbox">
+        <input name="lname" type="text" class="logintxtbox" id="lname" />
+      </span></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="right">Address</td>
+      <td align="right">&nbsp;</td>
+      <td valign="top"><span class="logintxtbox">
+        <input name="addr" type="text" class="logintxtbox" id="addr" value="" />
+      </span></td>
+      <td valign="top" class="small">*</td>
+    </tr>
+    <tr>
+      <td align="right">Postal Code</td>
+      <td align="right">&nbsp;</td>
+      <td valign="top"><span class="logintxtbox">
+        <input name="pcode" type="text" class="logintxtbox" id="pcode" value="" />
+      </span></td>
+      <td valign="top" class="small">*</td>
+    </tr>
+    <tr>
+      <td align="right">Country</td>
+      <td align="right">&nbsp;</td>
+      <td><form id="form4" name="form4" method="post" action="">
+        <label for="country"></label>
+        <select name="country" size="1" class="logintxtbox" id="country">
+        </select>
+      </form></td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td class="small">*optional</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td width="272">&nbsp;</td>
+      <td width="87">&nbsp;</td>
+    </tr>
+  </table>
+
+  <p>
+
+  <table width="700" border="0" class="tbl">
+    <tr>
+      <td width="344" align="center" class="small"><p>
+        <input name="submit" type="submit" class="logintxtbox" id="submit" value="SUBMIT" />
+      </p></td>
+      <td width="346" align="center"><form id="form3" name="form3" method="post" action="">
+        <input name="reset" type="submit" class="logintxtbox" id="reset" value="RESET" />
+      </form></td>
+    </tr>
+  </table>
+  <p>
+  <!-- InstanceEndEditable --></div>
+<div style="position:relative;top:65px;" align="center"> REVERSI.COM 2015 ALL RIGHT RESERVED
+</div>
+</body>
+<!-- InstanceEnd --></html>
