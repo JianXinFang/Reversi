9b081d45c758a42dd676f6f51bc8b06fd16c483c
mrjohanosman <mrjohanosman@gmail.com>
2015-04-14 20:42:02 +0800

    Version 2
    Created about us and login page

---
+++ about.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/normal_page.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="styles.css" rel="stylesheet" type="text/css" />
+<title>Reversi</title>
+</head>
+
+<body>
+<div style="position:relative;top:0px;" align="center">
+  <table width="1200" border="0">
+    <tr>
+      <td width="970"><span style="text-align: left; font-size: 30pt;"><img src="imgs/left arrow.png" width="75" height="27" alt="previous" />&nbsp;<a href="news.php">Welcome to reversi!</a>&nbsp;<img src="imgs/right arrow.png" width="75" height="27" alt="next" /></span></td>
+      <td width="80"><img src="imgs/profile_icon.png" width="80" height="62" alt="profiles" /></td>
+      <td width="142"><span style="text-align: left; font-size: 30pt;"><a href="user_profiles.php">User<br />Profiles</a></span>
+      </td>
+    </tr>
+  </table>
+</div>
+<div style="position:relative;top:20px;" align="center">
+  <table width="1117" border="0">
+    <tr>
+      <td width="105"><span style="text-align: left; font-size: 30pt;"><a href="home.php">Home</a>&nbsp;</span></td>
+      <td width="10"><img src="imgs/menu div.png" width="10" height="50" /></td>
+      <td width="193"><span style="text-align: left; font-size: 30pt;">&nbsp;<a href="dashboard.php">Dashboard&nbsp;</a></span></td>
+      <td width="10"><img src="imgs/menu div.png" alt="" width="10" height="50" /></td>
+      <td width="209"><span style="text-align: left; font-size: 30pt;">&nbsp;<a href="tournament.php">Tournament</a>&nbsp;</span></td>
+      <td width="10"><img src="imgs/menu div.png" alt="" width="10" height="50" /></td>
+      <td width="156"><span style="text-align: left; font-size: 30pt;">&nbsp;<a href="ranking.php">Ranking</a>&nbsp;</span></td>
+      <td width="10"><img src="imgs/menu div.png" alt="" width="10" height="50" /></td>
+      <td width="97"><span style="text-align: left; font-size: 30pt;">&nbsp;<a href="faq.php">FAQ</a>&nbsp;</span></td>
+      <td width="10"><img src="imgs/menu div.png" alt="" width="10" height="50" /></td>
+      <td width="196"><span style="text-align: left; font-size: 30pt;">&nbsp;<a href="abtus.php">About Us</a></span></td>
+    </tr>
+  </table>
+</div>
+
+<div style="position:relative;top:65px;" align="center"><img src="imgs/reversilogo.png" width="800" height="198" alt="REVERSI" /></div>
+</div>
+<div style="position:relative;top:65px;height:1400px;" align="center"><!-- InstanceBeginEditable name="content_area" -->              
+  <h1><strong>Who Are We?</strong></h1>
+  <p>Our team consist of 4 members Fang JianXin, Albe rt Li, Johan Osman and Toh Shin Ee. <br />
+  We have a great team with different expertise in different IT technology. <br />
+  Below are biography of individual team members.</p>
+  <table width="1000" border="0">
+    <tr>
+      <td width="200" align="left" valign="top"><p align="left"><strong>Name: Fang JianXin<br />
+      Student No:4877159</strong></p></td>
+      <td width="790" align="left" valign="top"><div>
+        <p>Graduated from Temasek Polytechnic in year 2011 and he current working as a software developer in an energy base company and his company provides energy solution to both local and oversea companies.</p>
+      </div>
+        <div></div>
+      <div>
+        <p>He has been working with current company for almost 3 years and experience with 2 full systems development life cycle (SDLC). He had involved in some of his company core projects, both client and web base. These projects uses different development framework such as ASP.NET, Silverlight and Windows Presentation Foundation. He had experience with Windows Communication and Foundation and HTTP POST for communicating with other vendor&rsquo;s project to download and send them data through Public Key Infrastructure. Lastly, he also hasexperience in using Microsoft SQL Database.      <br />
+        </p>
+      </div></td>
+    </tr>
+    <tr>
+      <td height="41" colspan="2" align="right" valign="top"><img src="imgs/div2.png" alt="div" width="800" height="26" align="middle" /></td>
+    </tr>
+    <tr>
+      <td align="left" valign="top"><strong>Name: Johan Bin Osman</strong><br />        <strong>Student No: 4703200</strong></td>
+      <td align="left" valign="top"><div>Johan graduated from Ngee Ann Polytechnic with a diploma in Information Technology, specializing in Network and Security. He have been in the workforce for 3 years and have gain a wide range of knowledge and experience in the field of Network Security and System</div>
+        <div>administration.</div>
+        <div><br />
+        </div>
+        <div>Due to the nature of his job, Johan is skillful in System management including AD, DNS, DHCP, IIS, Exchange, NAS, Firewall and the wide range of TCP/IP protocol required in network management.</div>
+        <div></div>
+      <div>
+        <p>His main strength is the great ability working in a team. He works well under pressure and has a great skill in documentation.       <br />
+        </p>
+      </div></td>
+    </tr>
+    <tr>
+      <td height="34" colspan="2" align="right" valign="top"><img src="imgs/div2.png" alt="div" width="800" height="26" align="middle" /></td>
+    </tr>
+    <tr>
+      <td align="left" valign="top"><p><strong>Name: Li Jinyi</strong><br />
+          <strong>Student No.: 4592001</strong></p></td>
+      <td align="left" valign="top"><div>
+        <p>Graduated from Ngee Ann Polytechnic, with a diploma in Information</p>
+      </div>
+      <div>
+        <p>Technology specialized in e-Business. 5 years working experience as an IT project executive with experience in HTML, CSS, Javescript, Flash Action Script, PHP and MySQL Server. Clients include Nokia, HTC, Dell, NTU and Samsung.      </p>
+      </div></td>
+    </tr>
+    <tr>
+      <td height="40" colspan="2" align="right" valign="top"><img src="imgs/div2.png" alt="div" width="800" height="26" align="middle" /></td>
+    </tr>
+    <tr>
+      <td align="left" valign="top"><strong>Name:Toh Shin Ee</strong><br />        <strong>Student No:4442945</strong></td>
+      <td align="left" valign="top"><div>Shinee is a graduate from Republic Polytechnic with a diploma in Business Information Systems. She has working experience with data-related operations in a well-known Singapore maps company as well as</div>
+      <div>programming knowledge in HTML, PHP, C++ and Java. She currently works part-time as a sales coordinator in a stocks related company and enjoys playing video games in her spare time. </div></td>
+    </tr>
+  </table>
+  <p>&nbsp;</p>
+<!-- InstanceEndEditable --></div>
+<div style="position:relative;top:65px;" align="center"> REVERSI.COM 2015 ALL RIGHT RESERVED
+</div>
+</body>
+<!-- InstanceEnd --></html>
